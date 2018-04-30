8d86938e50d361b1979ee1e1548baa59307ce76e
slau8 <slau8@stuy.edu>
2018-04-30 15:31:49 -0400

    one more

--- reduce.py
+++ reduce.py
@@ -7,11 +7,17 @@ BOOK = story.split()
 
 BOOK = [x.strip(".,!:;?\"'()[]/-").lower() for x in BOOK]
 
+# Return frequency of string word 
 def word_freq(word):
-    return len([x for x in BOOK if x == word.lower()])
+    return len([i for i in BOOK if i == word.lower()])
 
+# Return total frequency of list words
 def words_freq(words):
-    return len([x for x in BOOK for y in words if x == y.lower()])
+    return reduce((lambda x,y: x + y), [word_freq(i) for i in words])  
+
+# Return most frequent word
+def top_word():
+    pass
 
 #print story
 print word_freq("Dorian")
