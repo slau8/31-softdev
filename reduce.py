5930091d093a25bad5912e5450131be13cc17109
Helen Ye <00yhelen@gmail.com>
2018-04-30 21:58:59 -0400

    I'm pretty sure this work but it's still running top word on the book

--- reduce.py
+++ reduce.py
@@ -7,19 +7,26 @@ BOOK = story.split()
 
 BOOK = [x.strip(".,!:;?\"'()[]/-").lower() for x in BOOK]
 
+TEST = ["word", "word", "word", "test", "the", "time", "time"]
+
 # Return frequency of string word 
-def word_freq(word):
-    return len([i for i in BOOK if i == word.lower()])
+def word_freq(word, l):
+    return reduce(lambda x, y: x + y, [1 for i in l if i == word.lower()])
 
 # Return total frequency of list words
-def words_freq(words):
-    return reduce((lambda x,y: x + y), [word_freq(i) for i in words])  
+def words_freq(words, l):
+    return reduce((lambda x,y: x + y), [word_freq(i, l) for i in words])
 
 # Return most frequent word
-def top_word():
-    pass
+def top_word(l):
+    return reduce((lambda x, y: x if word_freq(x, l) > word_freq(y, l) else y), l)
 
 #print story
-print word_freq("Dorian")
-print word_freq("Gray")
-print words_freq(["Dorian","Gray"])
+print word_freq("Dorian", BOOK)
+print word_freq("Gray", BOOK)
+print words_freq(["Dorian","Gray"], BOOK)
+print word_freq("word", TEST)
+print word_freq("test", TEST)
+print words_freq(["time","the"], TEST)
+print top_word(TEST)
+print top_word(BOOK)
